4871f2dfc3c5009da307588ffb7c6300b853241a
Vinish <vini30876@gmail.com>
2021-10-30 17:31:52 +0300

    Added open app functionality, active/passive mode, taskwriter etc
    Made passive_recognizer function to handle passiv mode and added calibrate mic task

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import speech_recognition as sr
 import datetime
 import webbrowser
 import os
-import harvy_tasks as ht
+import task_manager as tmgr
+from tdprocessor import taskwriter
 
 
 MASTER = "Vinish"
@@ -38,7 +39,9 @@ def wishMe():
 
 # Takes command from the microphone(STT)
 r = sr.Recognizer()
-def recognizer(mic_input):
+def active_recognizer(mic_input):
+    '''Returns text from the given audio argument. 
+    Should be used only when HARVI should be executing tasks.'''
     print("Listening.....")
     audio = r.listen(mic_input)
     try:
@@ -50,31 +53,55 @@ def recognizer(mic_input):
         query = 'None'
     return query.lower()
 
-def is_calibrate(bool : bool):
-    with sr.Microphone() as source:
-        if bool == True:
-            speak("Initializing Jarvis...")
-            r.adjust_for_ambient_noise(source, 5) 
-            speak("Initialized Jarvis...")
-            return recognizer(source)
-        elif bool == False:
-            return recognizer(source)
+def passive_recognizer(mic_input):
+    '''Used when HARVI shouldn't be executing any tasks'''
+    print("Passive Mode")
+    audio = r.listen(mic_input)
+    try:
+        print("Recognizing passively......")
+        query = r.recognize_google(audio, language= "en-in")
+    except Exception as e:
+        query = 'None'
+    return query.lower()
+
+
+
+def mic(listen = True, calibrate = True):
+    '''This function takes care of everything related to mic'''
+    if listen == True:
+        with sr.Microphone() as source:
+            if calibrate == True:
+                speak("Calibrating the mic")
+                r.adjust_for_ambient_noise(source, 3) 
+                speak("Ready to listen")
+                return active_recognizer(source)
+            elif calibrate == False:
+                return active_recognizer(source)
+    elif listen == False:
+        with sr.Microphone() as source:
+            return passive_recognizer(source)
+
+
+
 
 if __name__ == "__main__":
     wishMe()
+    listen = True
     calibrate = True  
+
     while True: #Logic for executing tasks
-        if calibrate == True:
-            query = is_calibrate(calibrate)
+        if calibrate == True and listen == True:
+            query = mic(calibrate=calibrate)
             calibrate = False
         else:
-            query = is_calibrate(calibrate)
-        print(query)
+            query = mic(listen=listen, calibrate=calibrate)
 
-        if query != 'None':
-            if 'wikipedia' in query:
+        if query != 'None' and listen == True:
+            if 'recalibrate mic' in query:
+                mic(calibrate=True)
+            elif 'wikipedia' in query:
                 speak("Searching Wikipedia.....")
-                speak(ht.wikipedia1(query))
+                speak(tmgr.wikipedia1(query))
             elif 'open youtube' in query:
                 url = "youtube.com"
                 browser_path = 'C:/Program Files/Mozilla Firefox/firefox.exe %s'
@@ -83,22 +110,38 @@ if __name__ == "__main__":
                 url = "google.com"
                 browser_path = 'C:/Program Files/Mozilla Firefox/firefox.exe %s'
                 webbrowser.get(browser_path).open(url)
-
             elif 'play music' in query:
                 songs = os.listdir(SONGS_DIR)
                 print(songs)
                 os.startfile(os.path.join(SONGS_DIR, songs[0]))
-
             elif 'the time' in query:
                 strTime = datetime.datetime.now().strftime("%H:%M:%S")
                 speak(f'{MASTER} the time is {strTime}')
-
-            elif 'open code' in query:
-                codePath = 'C:\\Users\\Vinish\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe'
-                os.startfile(codePath)
+            elif 'open app' in query:
+                query = query [query.index("open app") + len("open app"): ].strip()
+                print(query)
+                if len(query) >= 1:
+                    tmgr.openapp(query)
+            elif 'add apps' in query:
+                taskwriter()
+            elif 'stop listening' in query:
+                listen = False
             elif 'bye' in query:
                 print("Good Bye Sir")
                 speak("Good Bye Sir")
                 break
+        elif listen == False:
+            if query in ['listen to me', 'start listening']:
+                listen = True
         else:
             continue
+
+
+
+
+
+
+
+
+
+   
